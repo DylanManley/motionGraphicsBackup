4861239b3e3854ebe21c46e27957f2c6325efe76
Dylan Manley <c00286789@itcarlow.ie>
2024-02-21 11:37:16 +0000

    added first graphics

---
+++ OneButtonPlatformer/OneButtonPlatformer/Game.cpp
@@ -0,0 +1,236 @@
+/// <summary>
+/// @author Dylan Manley
+///
+
+
+#include "Game.h"
+#include <iostream>
+
+
+Game::Game() :
+	m_window{ sf::VideoMode{ 1000U, 800U, 32U }, "SFML Game" },
+	m_exitGame{false} 
+{
+	setup();
+}
+
+
+Game::~Game()
+{
+}
+
+
+void Game::run()
+{	
+	sf::Clock clock;
+	sf::Time timeSinceLastUpdate = sf::Time::Zero;
+	const float fps{ 60.0f };
+	sf::Time timePerFrame = sf::seconds(1.0f / fps); // 60 fps
+	while (m_window.isOpen())
+	{
+		processEvents(); // as many as possible
+		timeSinceLastUpdate += clock.restart();
+		while (timeSinceLastUpdate > timePerFrame)
+		{
+			timeSinceLastUpdate -= timePerFrame;
+			processEvents(); // at least 60 fps
+			update(timePerFrame); //60 fps
+		}
+		render(); // as many as possible
+	}
+}
+
+void Game::processEvents()
+{
+	sf::Event newEvent;
+	while (m_window.pollEvent(newEvent))
+	{
+		if ( sf::Event::Closed == newEvent.type) // window message
+		{
+			m_exitGame = true;
+		}
+		if (sf::Event::KeyPressed == newEvent.type) //user pressed a key
+		{
+			processKeys(newEvent);
+		}
+	}
+}
+
+void Game::processKeys(sf::Event t_event)
+{
+	if (sf::Keyboard::Escape == t_event.key.code)
+	{
+		m_exitGame = true;
+	}
+}
+
+/// <summary>
+/// Update the game world
+/// </summary>
+/// <param name="t_deltaTime">time interval per frame</param>
+void Game::update(sf::Time t_deltaTime)
+{
+	if (m_exitGame)
+	{
+		m_window.close();
+	}
+
+	handleInput();
+	Collisions();
+
+	for (int row = 0; row < numRows; row++)
+	{
+		for (int col = 0; col < numCols; col++)
+		{
+
+			level[row][col].move(-3.7, 0);
+		}
+
+	}
+
+	velocityY = velocityY + gravity;
+	player.move(0, velocityY);
+	gravity = 0.6;
+
+}
+
+
+void Game::render()
+{
+	m_window.clear(LightOrange);
+	for (int row = 0; row < numRows; row++)
+	{
+		for (int col = 0; col < numCols; col++)
+		{
+			m_window.draw(level[row][col]);
+		}
+	}
+	m_window.draw(player);
+	m_window.display();
+}
+
+void Game::setup()
+{
+	if (!m_ArialBlackfont.loadFromFile("ASSETS\\FONTS\\ariblk.ttf"))
+	{
+		std::cout << "problem loading arial black font" << std::endl;
+	}
+
+	groundTexture.loadFromFile("ASSETS\\IMAGES\\Ground_Tiles.jpg");
+	eyeTexture.loadFromFile("ASSETS\\IMAGES\\EyeTile.png");
+	
+	createLevel();
+	view = window.getDefaultView();
+	player.setSize(sf::Vector2f(20, 20));
+	player.setPosition(160, 500);
+	player.setFillColor(Wenge);
+	isJumping = false;
+
+}
+
+void Game::createLevel()
+{
+	for (int row = 0; row < numRows; row++)
+	{
+		for (int col = 0; col < numCols; col++)
+		{
+
+			if (levelData[row][col] == 1)
+			{
+
+				level[row][col].setSize(sf::Vector2f(70, 30));
+				level[row][col].setPosition(row * 70, col * 30);
+				level[row][col].setFillColor(UltraViolet);
+				level[row][col].setTexture(&groundTexture);
+			}
+			if (levelData[row][col] == 0)
+			{
+
+				level[row][col].setSize(sf::Vector2f(70, 30));
+				level[row][col].setPosition(row * 70, col * 30);
+				level[row][col].setFillColor(LightOrange);
+			}
+			if (levelData[row][col] == 2)
+			{
+				level[row][col].setSize(sf::Vector2f(70, 30));
+				level[row][col].setPosition(row * 70, col * 30);
+				level[row][col].setTexture(&eyeTexture);
+				level[row][col].setFillColor(RussianViolet);
+
+			}
+
+		}
+	}
+}
+
+void Game::handleInput()
+{
+	if (isJumping == false)
+	{
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+		{
+			velocityY = -11.8;
+			isJumping = true;
+		}
+
+		if (gravity == 0)
+		{
+			isJumping = false;
+		}
+	}
+}
+
+void Game::Collisions()
+{
+	for (int row = 0; row < numRows; row++)
+	{
+		for (int col = 0; col < numCols; col++)
+		{
+			if (velocityY >= 0)
+			{
+				if (levelData[row][col] == 1)
+				{
+
+					if (player.getGlobalBounds().intersects(level[row][col].getGlobalBounds()))
+					{
+						if (player.getPosition().y < level[row][col].getPosition().y)
+						{
+							gravity = 0;
+							velocityY = 0;
+							player.setPosition(player.getPosition().x, level[row][col].getPosition().y);
+							player.move(0, -player.getGlobalBounds().height);
+							isJumping = false;
+							break;
+						}
+						else {
+							setup();
+						}
+					}
+
+
+				}
+
+			}
+
+			if (velocityY < 0)
+			{
+				if (levelData[row][col] == 1)
+				{
+					if (player.getGlobalBounds().intersects(level[row][col].getGlobalBounds()))
+					{
+					}
+
+				}
+
+			}
+			if (levelData[row][col] == 2)
+			{
+				if (player.getGlobalBounds().intersects(level[row][col].getGlobalBounds()))
+				{
+					setup();
+				}
+			}
+		}
+	}
+
+}
